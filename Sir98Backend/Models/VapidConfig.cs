636dfd03bfd132d996c12042b8d5c8a73a8baab6
CharlineBennekou <149783592+CharlineBennekou@users.noreply.github.com>
2025-11-14 13:42:25 +0100

    Testing of notifications.

---
+++ Sir98Backend/Models/VapidConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace Sir98Backend.Models
+{
+    public class VapidConfig
+    {
+        //Normally, you would not hardcode these values but load them from a secure configuration source..
+        public string Subject { get; set; } = "mailto:you@example.com"; // contact email
+        public string PublicKey { get; set; } = "YOUR_PUBLIC_VAPID_KEY";
+        public string PrivateKey { get; set; } = "YOUR_PRIVATE_VAPID_KEY";
+    }
+}
