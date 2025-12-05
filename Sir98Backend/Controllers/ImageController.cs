cbb665d221bff68d1f582cd2147237b74b548e74
Mathias Nørgaard <150593990+MathiasNoergaard@users.noreply.github.com>
2025-12-05 09:14:59 +0100

    Added images and a controller for retrieving images (#5)
    Co-authored-by: MathiasNoergaard <man010@edu.zealand.dk>

---
+++ Sir98Backend/Controllers/ImageController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sir98Backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class ImageController : Controller
+    {
+        /// <summary>
+        /// Controller for requesting images
+        /// </summary>
+        /// <param name="image">Must be filename and extension like this: Mette.jpg</param>
+        /// <returns></returns>
+        [HttpGet("{image}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status415UnsupportedMediaType)]
+        public IActionResult GetImage(string image)
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string path = Path.Join(workingDirectory, "Images", $"{image}");
+            string directory = Path.Join(workingDirectory, "Images");
+            if (Directory.Exists(directory) && System.IO.File.Exists(path))
+            {
+                string extension = Path.GetExtension(path);
+                Byte[] b = System.IO.File.ReadAllBytes(path);
+                switch (extension)
+                {
+                    case ".png":
+                        return File(b, "image/png");
+                    case ".jpg":
+                    case ".jpeg":
+                    case ".jfif":
+                    case ".pjpeg":
+                    case ".pjp":
+                        return File(b, "image/jpeg");
+                    case ".svg":
+                        return File(b, "image/svg+xml");
+                    case ".tif":
+                        return File(b, "image/tiff");
+                    case ".avif":
+                        return File(b, "image/avif");
+                    default:
+                        return StatusCode(415, "The requested file type is not supported");
+                }
+            }
+            else
+            {
+                return NotFound($"Image file not found at {directory} {path}");
+            }
+        }
+    }
+}
