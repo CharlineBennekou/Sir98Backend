d7db295b5f60d5874a3e52d43402cacf32e9efaa
CharlineBennekou <149783592+CharlineBennekou@users.noreply.github.com>
2025-12-18 12:24:34 +0100

    Fixed the controller to be more consistent. Added Dtos to make logic simpler but still consistent. (#21)

--- Sir98Backend/Models/ActivitySubscription.cs
+++ Sir98Backend/Models/ActivitySubscription.cs
@@ -10,14 +10,15 @@
         public int ActivityId { get; set; }
         public DateTimeOffset OriginalStartUtc { get; set; } //Normal activities can be identified by just the ActivityId, but recurring activities need the StartUtc to identify the specific occurrence. This *needs* to be the original start time of the occurrence.
 
+        public bool AllOccurrences { get; set; }
+
+
+
         // Navigation property to the Activity
         public Activity? Activity { get; set; }
 
         //Navigation property to the User
         public User? User { get; set; }
-
-
-        public bool AllOccurrences { get; set; }
 
     }
 }
--- Sir98Backend/Models/DataTransferObjects/ActivitySubscriptionPostDto.cs
+++ Sir98Backend/Models/DataTransferObjects/ActivitySubscriptionPostDto.cs
@@ -1,10 +1,10 @@
 ﻿namespace Sir98Backend.Models.DataTransferObjects
 {
-    public class SubscribeRequestDto
+    public class ActivitySubscriptionPostDto
     {
         public string UserId { get; set; } = string.Empty;
         public int ActivityId { get; set; }
-        public DateTimeOffset? OriginalStartUtc { get; set; }
+        public DateTimeOffset OriginalStartUtc { get; set; }
         public bool AllOccurrences { get; set; }
     }
 }
