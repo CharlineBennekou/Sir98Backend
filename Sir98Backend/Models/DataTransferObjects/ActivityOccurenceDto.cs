b09308d29f91a0465d2c8278c999f8ce17e156da
MathiasNoergaard <man010@edu.zealand.dk>
2026-01-22 10:43:56 +0100

    Nullable property

--- Sir98Backend/Models/DataTransferObjects/ActivityOccurenceDto.cs
+++ Sir98Backend/Models/DataTransferObjects/ActivityOccurenceDto.cs
@@ -19,7 +19,7 @@
         public DateTimeOffset EndUtc { get; set; }
 
         public string Title { get; set; } = "";
-        public required string Address { get; set; }
+        public string? Address { get; set; }
         public string? Image { get; set; }
         public string? Link { get; set; }
         public string? Description { get; set; }
